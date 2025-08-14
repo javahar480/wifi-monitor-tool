8bd152fe464e5add519b9a598a4aa64c76536af7
javahar480 <ichigokurosakitenzo@gmail.com>
2025-08-14 20:15:58 +0530

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -6,8 +6,9 @@ if __name__ == "__main__":
     print("[*] Starting network monitoring...")
     t1 = Thread(target=run_device_monitor)
     t2 = Thread(target=run_traffic_sniffer)
+
     t1.start()
     t2.start()
+
     t1.join()
     t2.join()
-
